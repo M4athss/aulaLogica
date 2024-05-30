8ad64f4b0f8d30c1bf1cd82d23afdd8e83412f98
Matheus Assis <matheus-augustob@hotmail.com>
2024-05-29 21:57:48 -0300

    History1

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,16 @@
+
+
+
+
+function sortear(){
+    let qtd = document.getElementById('quantidade').value;
+    let inicia = document.getElementById('de').value;
+    let termina = document.getElementById('ate').value;
+
+    alert(qtd + inicia + termina);
+}
+
+function reiniciar(){
+    qtd, inicia, termina = 0;
+}
+
